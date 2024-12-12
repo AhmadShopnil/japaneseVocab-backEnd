32a989a517d71499b6821b69ecfeaa85675c2623
ahmadShopnil <ahmadshopnil75@gmail.com>
2024-12-12 22:59:36 +0600

    protect lessons

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import { userController } from './user.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
-router.put('/changeRole/:id', userController.changeUserRole);
+router.put(
+  '/changeRole/:id',
+  auth(USER_ROLE.admin),
+  userController.changeUserRole,
+);
 router.delete('/:id', userController.deleteSingleUserById);
-// router.get(
-//   '/me',
-//   auth(USER_ROLE.user, USER_ROLE.admin),
-//   userController.getMyProfile,
-// );
-
-router.get('/', userController.getAllUsers);
+router.get('/', auth(USER_ROLE.admin), userController.getAllUsers);
 
 export const userRoutes = router;
--- src/app/modules/vocabulary/vocabulary.route.ts
+++ src/app/modules/vocabulary/vocabulary.route.ts
@@ -1,12 +1,30 @@
 import express from 'express';
 import { vocabularyController } from './vocabulary.controller';
+import { USER_ROLE } from '../user/user.constant';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
-router.put('/:id', vocabularyController.updateVocabulary);
-router.delete('/:id', vocabularyController.deleteVocabulary);
-router.post('/', vocabularyController.createVocabulary);
-router.get('/:id', vocabularyController.getSingleVocabulary);
-router.get('/', vocabularyController.getAllVocabulary);
+router.put(
+  '/:id',
+  auth(USER_ROLE.admin),
+  vocabularyController.updateVocabulary,
+);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.admin),
+  vocabularyController.deleteVocabulary,
+);
+router.post('/', auth(USER_ROLE.admin), vocabularyController.createVocabulary);
+router.get(
+  '/:id',
+  auth(USER_ROLE.user, USER_ROLE.admin),
+  vocabularyController.getSingleVocabulary,
+);
+router.get(
+  '/',
+  auth(USER_ROLE.user, USER_ROLE.admin),
+  vocabularyController.getAllVocabulary,
+);
 
 export const vocabularyRoutes = router;
